06578075b2e5ed9c1a7caf18c738bdd169234d16
auwau22visg <auwau22visg@gmail.com>
2021-07-04 22:28:58 +0900

    submitボタンの検知

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 document.addEventListener("DOMContentLoaded", function () {
   console.log("all ready.");
 });
+
+// formの送信
+let submitBtn = document.getElementById("form");
+
+submitBtn.addEventListener("click", function () {
+  console.log("submit");
+});
